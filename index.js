a1169c050183675a0cd1918bdf00e09f89dacf55
Muakhir <almuakhirjones@gmail.com>
2024-02-12 16:24:48 +0200

    .

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-console.log('Hello world');
+import express  from "express";
+
+// express app
+
+const app = express();
+
+// router
+
+const router = express.Router();
+
+// port
+const port = +process.env.PORT || 4000
+
+// Json Url
+
+const dataURL = 'https://muakhir.github.io/vue-eomp-data/Data/'
+
+app.use(router)
+
+router.get('/', (req,res)=>{
+    res.json({status:res.statusCode, msg: "You're on the Home page"})
+})
+
+// fetch all education from data file
+
+router.get('/education', async (req,res)=>{
+    let {education} = await (await fetch(dataURL)).json()
+    res.json({status:res.statusCode, education})
+})
+
+app.listen(port,()=>console.log(`server is running on Port: ${port}`))
