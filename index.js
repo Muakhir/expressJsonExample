e87055c8ae856ad31910906ab8741176bd3c62eb
Muakhir <almuakhirjones@gmail.com>
2024-02-13 16:18:20 +0200

    .

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ import express  from "express";
 
 const app = express();
 
+// import axios
+
+import axios from "axios"
+
 // router
 
 const router = express.Router();
@@ -23,9 +27,38 @@ router.get('/', (req,res)=>{
 
 // fetch all education from data file
 
+// router.get('/education', async (req,res)=>{
+//     let {education} = await (await fetch(dataURL)).json()
+//     res.json({status:res.statusCode, education})
+// })
+
+// axios fetch
 router.get('/education', async (req,res)=>{
-    let {education} = await (await fetch(dataURL)).json()
-    res.json({status:res.statusCode, education})
+let response = await axios.get (dataURL)
+let {education} = await response.data
+res.json({status:res.statusCode, education})
 })
 
+router.post('/education', async (req,res)=>{
+    let response = await axios.post (dataURL,{
+        id : idx,
+        year: new Date().getFullYear(),
+        description: '' || req.body.description
+    }
+    )
+})
+
+router.get('/updateEducation', async(req, res) => {
+    let educationIdUpdate = req.params.id
+    res.json({ status: res.statusCode,msg: `Updated education: ${educationIdUpdate}` });
+});
+
+router.get('/deleteEducation', async(req, res) => {
+    let educationIdDelete = req.params.id
+    res.json({ status: res.statusCode, msg: `Delete education with ID: ${educationIdDelete}` });
+});
+
+
+router.patch
 app.listen(port,()=>console.log(`server is running on Port: ${port}`))
+
