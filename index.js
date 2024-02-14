95cc8a391ab1db40f9c95363564fe99621d5678e
Muakhir <almuakhirjones@gmail.com>
2024-02-14 12:02:05 +0200

    .

--- index.js
+++ index.js
@@ -8,6 +8,11 @@ const app = express();
 
 import axios from "axios"
 
+
+
+import bodyParser from "body-parser";
+import cors from "cors";
+
 // router
 
 const router = express.Router();
@@ -19,7 +24,9 @@ const port = +process.env.PORT || 4000
 
 const dataURL = 'https://muakhir.github.io/vue-eomp-data/Data/'
 
-app.use(router)
+app.use(express.json(),
+        express.urlencoded({extended:true}),
+        cors(),router)
 
 router.get('/', (req,res)=>{
     res.json({status:res.statusCode, msg: "You're on the Home page"})
@@ -33,30 +40,87 @@ router.get('/', (req,res)=>{
 // })
 
 // axios fetch
-router.get('/education', async (req,res)=>{
-let response = await axios.get (dataURL)
-let {education} = await response.data
-res.json({status:res.statusCode, education})
-})
+// router.get('/education', async (req,res)=>{
+// let response = await axios.get (dataURL)
+// let {education} = await response.data
+// res.json({status:res.statusCode, education})
+// })
 
-router.post('/education', async (req,res)=>{
-    let response = await axios.post (dataURL,{
-        id : idx,
-        year: new Date().getFullYear(),
-        description: '' || req.body.description
+// router.post('/education',bodyParser.json() ,async (req,res)=>{
+//     let response = await axios.post (dataURL,{
+//         id : idx,
+//         year: new Date().getFullYear(),
+//         description: '' || req.body.description
+//     }
+//     )
+// })
+
+// router.get('/updateEducation', async(req, res) => {
+//     let educationIdUpdate = req.body.id
+//     res.json({ status: res.statusCode,msg: `Updated education: ${educationIdUpdate}` });
+// });
+
+// router.get('/deleteEducation', async(req, res) => {
+//     let educationIdDelete = req.body.id
+//     res.json({ status: res.statusCode, msg: `Delete education with ID: ${educationIdDelete}` });
+// });
+
+router.get('/education', async (req, res)=>{
+    try{
+        let response = await axios.get(dataURL)
+        let {education} = await response.data
+        res.json({
+            status: res.statusCode,
+            education
+        })    
+    }catch(e){
+        res.json({
+            status: res.statusCode, 
+            msg: 'Please try again later.'
+        })
     }
-    )
 })
 
-router.get('/updateEducation', async(req, res) => {
-    let educationIdUpdate = req.params.id
-    res.json({ status: res.statusCode,msg: `Updated education: ${educationIdUpdate}` });
-});
 
-router.get('/deleteEducation', async(req, res) => {
-    let educationIdDelete = req.params.id
-    res.json({ status: res.statusCode, msg: `Delete education with ID: ${educationIdDelete}` });
-});
+router.post('/addEducation', 
+bodyParser.json(), async (req, res)=>{
+    try{
+        let dataRes = await 
+        axios.post(dataURL, req.body)
+        if(dataRes){
+            /*
+            Please remove all consoles before you can deploy your backend.
+            */
+            console.log(resVal.data);
+            res.json({
+                status: res.statusCode,
+                msg: 'New record was added'
+            })
+        }
+    }catch(e) {
+        console.log(e.message);
+        // 
+        res.json({
+            status: res.statusCode,
+            msg: 'An error occurred when adding a new data'
+        })
+    }
+
+})
+router.patch('/updateEducation/:id',bodyParser.json(), (req, res)=>{
+    // axios.patch(`${dataURL}`, )
+})
+router.delete('/deleteEducation/:id', 
+async (req, res)=>{
+    let dataRes = await axios.delete(`${dataURL}/${+req.params.id}`, )
+    if(dataRes) {
+        res.json({
+            status: res.statusCode,
+            msg: 'A record was removed'
+        })
+    }
+})
+
 
 
 router.patch
